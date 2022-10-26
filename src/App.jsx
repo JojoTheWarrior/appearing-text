98887686beb1504f85aada63dfc7fc1d6746fc49
JojoTheWarrior <blahblahblah123toronto@gmail.com>
2022-10-25 21:26:04 -0400

    i commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import Input from './Input';
 
 function App() {
   return (
     <>
-      <h1>Title</h1>
+      <h1>This is the title</h1>
       <Input />
     </>
   );
